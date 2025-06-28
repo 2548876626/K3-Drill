30b1e7e6e5634b209e87916bc3ebdc146cf2aac9
2548876626 <2548876626@qq.com>
2025-06-29 00:11:54 +0800

    docs: update all K3 question data

---
+++ diyashicao/K3-imge/0-download_images.bat
@@ -0,0 +1,50 @@
+@echo off
+chcp 65001 > nul
+echo ======================================================
+echo ==     批量图片下载脚本 (K3 第六批，共 11 张)     ==
+echo ======================================================
+echo.
+echo 图片将被下载到当前文件夹。
+echo 按任意键开始下载...
+pause >nul
+echo.
+
+echo [1/11] 正在下载: 20250517110934_1747451374r8BTjSnYJE.png
+curl "https://pubnewfr.paperol.cn/315743006/20250517110934_1747451374r8BTjSnYJE.png?x-oss-process=image/resize,w_600" -H "Referer: https://pubnewfr.paperol.cn/" -o "20250517110934_1747451374r8BTjSnYJE.png" --silent
+
+echo [2/11] 正在下载: 20250517110945_1747451385pX8Gd8H43T.png
+curl "https://pubnewfr.paperol.cn/315743006/20250517110945_1747451385pX8Gd8H43T.png?x-oss-process=image/resize,w_600" -H "Referer: https://pubnewfr.paperol.cn/" -o "20250517110945_1747451385pX8Gd8H43T.png" --silent
+
+echo [3/11] 正在下载: 20250517110953_1747451393cy7nQzi4hr.png
+curl "https://pubnewfr.paperol.cn/315743006/20250517110953_1747451393cy7nQzi4hr.png?x-oss-process=image/resize,w_600" -H "Referer: https://pubnewfr.paperol.cn/" -o "20250517110953_1747451393cy7nQzi4hr.png" --silent
+
+echo [4/11] 正在下载: 20250517111001_1747451401W3448ZeDsp.png
+curl "https://pubnewfr.paperol.cn/315743006/20250517111001_1747451401W3448ZeDsp.png?x-oss-process=image/resize,w_600" -H "Referer: https://pubnewfr.paperol.cn/" -o "20250517111001_1747451401W3448ZeDsp.png" --silent
+
+echo [5/11] 正在下载: 20250517111009_1747451409fwbyM4pHtp.png
+curl "https://pubnewfr.paperol.cn/315743006/20250517111009_1747451409fwbyM4pHtp.png?x-oss-process=image/resize,w_600" -H "Referer: https://pubnewfr.paperol.cn/" -o "20250517111009_1747451409fwbyM4pHtp.png" --silent
+
+echo [6/11] 正在下载: 20250517111228_1747451548FmkhmPECSP.png
+curl "https://pubnewfr.paperol.cn/315743006/20250517111228_1747451548FmkhmPECSP.png?x-oss-process=image/resize,w_600" -H "Referer: https://pubnewfr.paperol.cn/" -o "20250517111228_1747451548FmkhmPECSP.png" --silent
+
+echo [7/11] 正在下载: 20250517111236_17474515562HdEPXpQTf.png
+curl "https://pubnewfr.paperol.cn/315743006/20250517111236_17474515562HdEPXpQTf.png?x-oss-process=image/resize,w_600" -H "Referer: https://pubnewfr.paperol.cn/" -o "20250517111236_17474515562HdEPXpQTf.png" --silent
+
+echo [8/11] 正在下载: 20250517111246_1747451566h6cse5Schb.png
+curl "https://pubnewfr.paperol.cn/315743006/20250517111246_1747451566h6cse5Schb.png?x-oss-process=image/resize,w_600" -H "Referer: https://pubnewfr.paperol.cn/" -o "20250517111246_1747451566h6cse5Schb.png" --silent
+
+echo [9/11] 正在下载: 20250517111257_1747451577ttYeaFnWRC.png
+curl "https://pubnewfr.paperol.cn/315743006/20250517111257_1747451577ttYeaFnWRC.png?x-oss-process=image/resize,w_600" -H "Referer: https://pubnewfr.paperol.cn/" -o "20250517111257_1747451577ttYeaFnWRC.png" --silent
+
+echo [10/11] 正在下载: 20250628104015_1751078415WFxJaR7NSS.png
+curl "https://pubnewfr.paperol.cn/315743006/20250628104015_1751078415WFxJaR7NSS.png?x-oss-process=image/resize,w_600" -H "Referer: https://pubnewfr.paperol.cn/" -o "20250628104015_1751078415WFxJaR7NSS.png" --silent
+
+echo [11/11] 正在下载: 20250628104057_1751078457ydPtG4pAfc.png
+curl "https://pubnewfr.paperol.cn/315743006/20250628104057_1751078457ydPtG4pAfc.png?x-oss-process=image/resize,w_600" -H "Referer: https://pubnewfr.paperol.cn/" -o "20250628104057_1751078457ydPtG4pAfc.png" --silent
+
+echo.
+echo ======================================================
+echo ==                 所有图片下载完成！                 ==
+echo ======================================================
+echo.
+pause
